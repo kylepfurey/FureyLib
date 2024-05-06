3b8f22f0b50592ff3accf8d06a92a4f4a9363a94
Kyle Furey <kylepfurey@gmail.com>
2024-05-06 12:58:10 -0400

    Added Params to Main

--- FureyLib/Misc or Not Mine/C#/Main.cs
+++ FureyLib/Misc or Not Mine/C#/Main.cs
@@ -7,13 +7,16 @@ using System.Collections.Generic;
 
 namespace Application
 {
+    /// <summary>
+    /// The class representing the program that the application is running on.
+    /// </summary>
     public class Program
     {
         /// <summary>
         /// Entry point of the program
         /// </summary>
         /// <param name="args"></param>
-        public static void Main(string[] args)
+        public static void Main(params string[] args)
         {
             Console.WriteLine("Hello, Dinner!");
 

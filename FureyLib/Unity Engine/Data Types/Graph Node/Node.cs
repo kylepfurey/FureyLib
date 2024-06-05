b7a4839d93fbe0ee12e1714707f4c5aa172b9c3e
Kyle Furey <kylepfurey@gmail.com>
2024-06-04 20:36:22 -0400

    Merged Unity Node and Connection Scripts

--- FureyLib/Unity Engine/Data Types/Graph Node/Node.cs
+++ FureyLib/Unity Engine/Data Types/Graph Node/Node.cs
@@ -225,3 +225,305 @@ public class Node : MonoBehaviour
         return connections;
     }
 }
+
+/// <summary>
+/// Stores the connection of two nodes and the weight of the connection.
+/// </summary>
+[System.Serializable]
+public class Connection
+{
+    // CONNECTION DATA
+
+    /// <summary>
+    /// The node that owns this connection
+    /// </summary>
+    public Node from = null;
+
+    /// <summary>
+    /// The node that is being connected to by the from node
+    /// </summary>
+    public Node to = null;
+
+    /// <summary>
+    /// The weight of this connection
+    /// </summary>
+    public float weight = 1;
+
+    /// <summary>
+    /// Whether this connection is active
+    /// </summary>
+    public bool active = true;
+
+
+    // CONNECTION CONSTRUCTORS
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public Connection()
+    {
+        from = null;
+        to = null;
+        weight = 1;
+        active = true;
+    }
+
+    /// <summary>
+    /// Connection constructor
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <param name="active"></param>
+    public Connection(Node from, Node to, float weight = 1, bool active = true)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+    }
+
+    /// <summary>
+    /// Connection constructor
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="active"></param>
+    /// <param name="weight"></param>
+    public Connection(Node from, Node to, bool active, float weight = 1)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+    }
+
+
+    // CONNECTION FUNCTIONS
+
+    /// <summary>
+    /// Reconnect a connection's from
+    /// </summary>
+    /// <param name="from"></param>
+    /// <returns></returns>
+    public Connection ReconnectFrom(Node from)
+    {
+        this.from = from;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection ReconnectFrom(Node from, float weight)
+    {
+        this.from = from;
+        this.weight = weight;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection ReconnectFrom(Node from, bool active)
+    {
+        this.from = from;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="weight"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection ReconnectFrom(Node from, float weight, bool active)
+    {
+        this.from = from;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="active"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection ReconnectFrom(Node from, bool active, float weight)
+    {
+        this.from = from;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's to
+    /// </summary>
+    /// <param name="to"></param>
+    /// <returns></returns>
+    public Connection ReconnectTo(Node to)
+    {
+        this.to = to;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's to
+    /// </summary>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection ReconnectTo(Node to, float weight)
+    {
+        this.to = to;
+        this.weight = weight;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's to
+    /// </summary>
+    /// <param name="to"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection ReconnectTo(Node to, bool active)
+    {
+        this.to = to;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's to
+    /// </summary>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection ReconnectTo(Node to, float weight, bool active)
+    {
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's to
+    /// </summary>
+    /// <param name="to"></param>
+    /// <param name="active"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection ReconnectTo(Node to, bool active, float weight)
+    {
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from and to
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <returns></returns>
+    public Connection Reconnect(Node from, Node to)
+    {
+        this.from = from;
+        this.to = to;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from and to
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection Reconnect(Node from, Node to, float weight)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from and to
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection Reconnect(Node from, Node to, bool active)
+    {
+        this.from = from;
+        this.to = to;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from and to
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="weight"></param>
+    /// <param name="active"></param>
+    /// <returns></returns>
+    public Connection Reconnect(Node from, Node to, float weight, bool active)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+
+    /// <summary>
+    /// Reconnect a connection's from and to
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="active"></param>
+    /// <param name="weight"></param>
+    /// <returns></returns>
+    public Connection Reconnect(Node from, Node to, bool active, float weight)
+    {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.active = active;
+
+        return this;
+    }
+}

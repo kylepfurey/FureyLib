09647dfd2372cb8709ca3719ec52e313db92db8a
Kyle Furey <kylepfurey@gmail.com>
2024-06-07 14:37:08 -0400

    Updated Main Files

---
+++ FureyLib/Misc or Not Mine/Java/Main.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgram {
+
+   /*
+    * This is my first java program.
+    * This will print 'Hello, Dinner!' as the output
+    */
+
+   public static void main(String[] args) {
+      System.out.println("Hello, Dinner!");
+   }
+}

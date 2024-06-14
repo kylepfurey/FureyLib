80af4d20441ed31c5644085e78862e54e983e604
Kyle Furey <kylepfurey@gmail.com>
2024-06-14 14:51:40 -0400

    Added Leak Detecting Scripts

--- FureyLib/Misc or Not Mine/C++/Main.cpp
+++ FureyLib/Misc or Not Mine/C++/Main.cpp
@@ -1,6 +1,7 @@
 
 // Program Main Entry Point Script
 
+#include <crtdbg.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,9 +11,12 @@ using namespace std;
 // Entry point of the program
 int main(int argc, char* argv[])
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_WNDW);
+
 	cout << "Hello, Dinner!" << endl;
 
 	// New code here
 
+	_CrtDumpMemoryLeaks();
 	return 0;
 }

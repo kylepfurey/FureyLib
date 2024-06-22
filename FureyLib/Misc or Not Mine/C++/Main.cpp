8fb08cb410a3f61743d0ae0365b6f050cbde9716
Kyle Furey <kylepfurey@gmail.com>
2024-06-22 12:02:35 -0400

    Updated Main Files

--- FureyLib/Misc or Not Mine/C++/Main.cpp
+++ FureyLib/Misc or Not Mine/C++/Main.cpp
@@ -7,20 +7,15 @@
 #include <array>
 #include <vector>
 #include <map>
-#include <crtdbg.h>
 
 using namespace std;
 
 // Entry point of the program
 int main(int argc, char* argv[])
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_WNDW);
-
 	cout << "Hello, Dinner!" << endl;
 
 	// New code here
 
-	_CrtDumpMemoryLeaks();
 	return 0;
 }
--- FureyLib/Misc or Not Mine/C/Main.c
+++ FureyLib/Misc or Not Mine/C/Main.c
@@ -5,18 +5,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <crtdbg.h>
 
 // Entry point of the program
 int main(int argc, char* argv[])
 {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_WNDW);
-
 	printf("Hello, Dinner!\n");
 
 	// New code here
 
-	_CrtDumpMemoryLeaks();
 	return 0;
 }

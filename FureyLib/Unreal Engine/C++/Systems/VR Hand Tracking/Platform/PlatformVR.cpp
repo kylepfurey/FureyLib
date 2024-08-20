ec912c0d289c94372d92cbbc8dd32742963eacdb
Kyle Furey <kylepfurey@gmail.com>
2024-08-20 00:00:18 -0400

    Added PlatformVR

---
+++ FureyLib/Unreal Engine/C++/Systems/VR Hand Tracking/Platform/PlatformVR.cpp
@@ -0,0 +1,118 @@
+
+// Static PC and Mobile VR Functions Script
+// by Kyle Furey
+
+// REQUIREMENT: PlatformVR.h
+
+// Include this heading to use the library
+#include "PlatformVR.h"
+
+
+// STATIC FUNCTIONS
+
+// Returns the name of the current platform.
+FString UPlatformVR::GetPlatform()
+{
+	return FPlatformProperties::PlatformName();
+}
+
+// Returns whether the current platform is a Mobile device (the game is playing on a VR headset).
+bool UPlatformVR::IsMobile()
+{
+#if IS_MOBILE
+
+	return true;
+
+#else
+
+	return false;
+
+#endif
+}
+
+// Returns whether the current platform is a PC (the game is playing on Windows, Mac, or Linux).
+bool UPlatformVR::IsPC()
+{
+#if IS_MOBILE
+
+	return false;
+
+#else
+
+	return true;
+
+#endif
+}
+
+// Returns whether the game is in editor mode.
+bool UPlatformVR::IsEditor()
+{
+	return GIsEditor;
+}
+
+// Multiply this value with a light's intensity value to normalize its brightness between PC and Mobile platforms.
+float UPlatformVR::GetLightScalar()
+{
+	if (GIsEditor)
+	{
+		return 1;
+	}
+
+#if IS_MOBILE
+
+#if MATCH_MOBILE_LIGHTING
+
+	return 1;
+
+#else
+
+	return PC_TO_MOBILE_LIGHT_INTENSITY_SCALAR;
+
+#endif
+
+#else
+
+#if MATCH_MOBILE_LIGHTING
+
+	return MOBILE_TO_PC_LIGHT_INTENSITY_SCALE;
+
+#else
+
+	return 1;
+
+#endif
+
+#endif
+}
+
+// Updates the intensity of a world's lights to ensure its brightness is consistent between platforms.
+// Returns whether the lights were successfully normalized and all of the lights in the world.
+void UPlatformVR::NormalizeLighting(bool& Success, TArray<ALight*>& Lights)
+{
+	if (!GIsEditor && GWorld != nullptr)
+	{
+		TArray<AActor*> Actors;
+
+		UGameplayStatics::GetAllActorsOfClass(GWorld, ALight::StaticClass(), Actors);
+
+		for (AActor* Actor : Actors)
+		{
+			ALight* Light = Cast<ALight>(Actor);
+
+			Lights.Add(Light);
+
+			ULightComponent* Component = Light->GetLightComponent();
+
+			Component->SetIntensity(Component->Intensity * GetLightScalar());
+
+			Success = true;
+		}
+
+		if (Success)
+		{
+			return;
+		}
+	}
+
+	Success = false;
+}
---
+++ FureyLib/Unreal Engine/C++/Systems/VR Hand Tracking/Platform/PlatformVR.h
@@ -0,0 +1,67 @@
+
+// Static PC and Mobile VR Functions Script
+// by Kyle Furey
+
+// REQUIREMENT: PlatformVR.cpp
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Engine/Light.h"
+#include "Components/LightComponent.h"
+#include "PlatformVR.generated.h"
+
+// Include this heading to use the library
+// #include "PlatformVR.h"
+
+// Whether the platform is a mobile platform.
+#define IS_MOBILE PLATFORM_ANDROID
+
+// Whether worlds are lit based on the Mobile preview and should be corrected accordingly.
+#define MATCH_MOBILE_LIGHTING 1
+
+// The scalar applied to a light's intensity in a scene lit in the PC preview to match the Mobile preview.
+#define PC_TO_MOBILE_LIGHT_INTENSITY_SCALAR 3.0
+
+// The scalar applied to a light's intensity in a scene lit in the Mobile preview to match the PC preview.
+#define MOBILE_TO_PC_LIGHT_INTENSITY_SCALE (1.0 / PC_TO_MOBILE_LIGHT_INTENSITY_SCALAR)
+
+/** Provides a library of functions used to match a VR game between PC and Android. */
+UCLASS(Blueprintable, BlueprintType)
+class MYGAME_API UPlatformVR : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	// STATIC FUNCTIONS
+
+	/** Returns the name of the current platform. */
+	UFUNCTION(BlueprintCallable, BlueprintPure = true, Category = "PlatformVR", meta = (Keywords = "Get, Platform"))
+	static FString GetPlatform();
+
+	/** Returns whether the current platform is a Mobile device (the game is playing on a VR headset). */
+	UFUNCTION(BlueprintCallable, BlueprintPure = true, Category = "PlatformVR", meta = (Keywords = "Is, Mobile, Oculus, Android, VR, Headset"))
+	static bool IsMobile();
+
+	/** Returns whether the current platform is a PC (the game is playing on Windows, Mac, or Linux). */
+	UFUNCTION(BlueprintCallable, BlueprintPure = true, Category = "PlatformVR", meta = (Keywords = "Is, PC, Windows, Mac, Linux"))
+	static bool IsPC();
+
+	/** Returns whether the game is in editor mode. */
+	UFUNCTION(BlueprintCallable, BlueprintPure = true, Category = "PlatformVR", meta = (Keywords = "Is, Editor, Edit, Mode"))
+	static bool IsEditor();
+
+	/** Multiply this value with a light's intensity value to normalize its brightness between PC and Mobile platforms. */
+	UFUNCTION(BlueprintCallable, BlueprintPure = true, Category = "PlatformVR", meta = (Keywords = "Get, Light, Scalar"))
+	static float GetLightScalar();
+
+	/**
+	* Updates the intensity of a world's lights to ensure its brightness is consistent between platforms.
+	* Returns whether the lights were successfully normalized and all of the lights in the world.
+	*/
+	UFUNCTION(BlueprintCallable, Category = "PlatformVR", meta = (Keywords = "Normalize, Lighting"))
+	static void NormalizeLighting(bool& Success, TArray<ALight*>& Lights);
+};

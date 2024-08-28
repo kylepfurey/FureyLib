bd3b446b37436c1f329837de2b3a3299f3675377
Kyle Furey <kylepfurey@gmail.com>
2024-08-27 21:37:11 -0400

    Added Factory

---
+++ FureyLib/C++/Data Types/factory.h
@@ -0,0 +1,34 @@
+﻿
+// Factory Class Script
+// by Kyle Furey
+
+#pragma once
+
+// Include this heading to use the class.
+#include "factory.h"
+
+// Provides three functions used for easy instantiation of derived objects.
+template<typename base_class = void> class factory
+{
+public:
+
+	// INSTANTIATION
+
+	// Instantiates a new object of the given derived type.
+	template <typename derived_type, typename ... argument_types> static derived_type instantiate(argument_types ... arguments)
+	{
+		return derived_type(arguments...);
+	}
+
+	// Instantiates a new derived object of the given template.
+	template <typename derived_type> static derived_type instantiate(derived_type _template)
+	{
+		return derived_type(_template);
+	}
+
+	// Instantiates a new derived object of the given template.
+	template <typename derived_type> static derived_type instantiate(derived_type* _template)
+	{
+		return derived_type(*_template);
+	}
+};

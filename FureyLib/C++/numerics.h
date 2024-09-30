a63efaad5e4a8c1f72132116b96c834d34929e5d
Kyle Furey <kylepfurey@gmail.com>
2024-09-30 15:33:51 -0400

    Added Numeric Types

---
+++ FureyLib/C++/numerics.h
@@ -0,0 +1,32 @@
+
+// Numeric Type Declarations Script
+// by Kyle Furey
+
+#pragma once
+#include <cstdint>
+#include <cmath>
+#include <climits>
+
+// Include this heading to unify numeric type sizes
+#include "numerics.h"
+
+// NUMERIC TYPES
+
+typedef     int8_t        int8;
+typedef     uint8_t       uint8;
+typedef     int16_t       int16;
+typedef     uint16_t      uint16;
+typedef     int32_t       int32;
+typedef     uint32_t      uint32;
+typedef     int64_t       int64;
+typedef     uint64_t      uint64;
+typedef     float_t       float32;
+typedef     double_t      float64;
+typedef     wchar_t       wchar;
+typedef     char8_t       char8;
+typedef     char16_t      char16;
+typedef     char32_t      char32;
+typedef     intptr_t      intptr;
+typedef     uintptr_t     uintptr;
+typedef     nullptr_t     null;
+typedef     size_t        size;

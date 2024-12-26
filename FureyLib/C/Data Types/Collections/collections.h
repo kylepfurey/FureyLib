23090821d8da235ed17e036ed03ba7296f5f30ec
Kyle Furey <kylepfurey@gmail.com>
2024-12-26 17:42:10 -0500

    Added Collections Header

---
+++ FureyLib/C/Data Types/Collections/collections.h
@@ -0,0 +1,20 @@
+// .h
+// C Collection Types Header Script
+// by Kyle Furey
+
+#pragma once
+
+// C STANDARD LIBRARY
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+// C COLLECTIONS
+
+#include "vector/vector.h"
+#include "list/list.h"
+#include "set/set.h"
+#include "map/map.h"

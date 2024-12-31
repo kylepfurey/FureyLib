222a1f6b97d6617b535be6cae32b1e84b02c50c4
Kyle Furey <kylepfurey@gmail.com>
2024-12-31 15:46:41 -0500

    Added Rust Struct Example

---
+++ FureyLib/Misc or Not Mine/Rust/Boilerplates/struct.rs
@@ -0,0 +1,79 @@
+
+// Name
+// Creator
+
+use std::mem::*;
+use std::rc::*;
+use std::boxed::*;
+use std::io::*;
+use std::string::*;
+use std::vec::*;
+use std::collections::*;
+use std::ops::*;
+
+// Include this heading to use the class
+// mod r#struct;
+// use r#struct::*;
+
+
+// STRUCTURE
+
+/** Template structure script. */
+pub struct Struct {
+    // VARIABLES
+
+    // Example x variable.
+    x: f32,
+
+    // Example y variable.
+    y: f32,
+
+    // New variables here
+}
+
+
+// FUNCTIONS
+impl Struct {
+    // CONSTRUCTORS
+
+    /** Initializes a new structure. */
+    pub fn new(x: f32, y: f32) -> Self {
+        Struct { x, y }
+    }
+
+    // New constructors here
+
+
+    // METHODS
+
+    /** Returns the magnitude of the given structure. */
+    pub fn magnitude(&self) -> f32 {
+        if self.x == 0.0 || self.y == 0.0 {
+            return 0.0;
+        }
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    /** Normalizes the given structure. */
+    pub fn normalize(&mut self) -> &mut Self {
+        let magnitude = self.magnitude();
+        if magnitude == 0.0 {
+            return self;
+        }
+        self.x /= magnitude;
+        self.y /= magnitude;
+        self
+    }
+
+    // New methods here
+}
+
+
+// DESTRUCTOR
+impl Drop for Struct {
+    /** Properly destroys the given structure. */
+    fn drop(&mut self) {
+        self.x = 0.0;
+        self.y = 0.0;
+    }
+}

aceb4ed955650e19d0c0fe35d7d0873b7406a3c2
Kyle Furey <kylepfurey@gmail.com>
2025-02-23 11:37:54 -0500

    Upgraded Image Animator

--- FureyLib/Unity Engine/Individual Components/Utility/ImageAnimator.cs
+++ FureyLib/Unity Engine/Individual Components/Utility/ImageAnimator.cs
@@ -1,50 +1,179 @@
-
-// UI Image Animator Script
+// .cs
+// 2D Image Animator Component
 // by Kyle Furey
 
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// Animates an image through a set of textures.
+/// Plays a sequence of images over time to make an image animate.
 /// </summary>
-public class ImageAnimator : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public sealed class ImageAnimator : MonoBehaviour
 {
-    [Header("Animates an image through a set of textures.\n")]
+    [Header("Plays a sequence of images over time to make an image animate.\n")]
 
-    [Header("The image to animate:")]
-    [SerializeField] private RawImage image = null;
+    [Header("The managed image used to create animations:")]
+    public Image image = null;
 
-    [Header("The textures to loop through:")]
-    [SerializeField] private List<Texture> textures = new List<Texture>();
+    [Header("The animation to start playing on start:")]
+    [SerializeField] private string startAnim = "Idle";
 
-    [Header("The speed to iterate through the textures:")]
-    [SerializeField] private float speed = 30;
+    [Header("The number of animation frames to play per second:")]
+    [SerializeField] private float frameRate = 30;
 
-    [Header("Whether to animate the textures:")]
-    public bool animate = true;
+    [Header("Each animation by name:")]
+    [SerializeField] private List<ImageAnimation> animations = new List<ImageAnimation>();
+
+
+    // FIELDS
 
     /// <summary>
-    /// The current index of the textures.
+    /// The current index of the current animation.
     /// </summary>
-    private float index = 0;
+    private int animIndex = 0;
 
     /// <summary>
-    /// Initializes the animation.
+    /// The current animation being played.
+    /// </summary>
+    private ImageAnimation currentAnim = null;
+
+    /// <summary>
+    /// The number of times the animation has changed.
+    /// </summary>
+    private int animCount = 0;
+
+
+    // PROPERTIES
+
+    /// <summary>
+    /// Returns the name of the current animation or sets the current animation by name.<br/>
+    /// This property does not overwrite the same playing animation.
+    /// </summary>
+    public string Animation
+    {
+        get
+        {
+            return currentAnim.name;
+        }
+
+        set
+        {
+            if (value.ToLower() != currentAnim.name.ToLower())
+            {
+                SetAnimation(value);
+            }
+        }
+    }
+
+
+    // UNITY EVENTS
+
+    /// <summary>
+    /// Awake() is called before Start().
+    /// </summary>
+    private void Awake()
+    {
+        if (image == null)
+        {
+            image = GetComponent<Image>();
+
+            if (image == null)
+            {
+                Debug.LogError("ERROR: No image was found for the ImageAnimator on \"" + gameObject.name + "\"!");
+
+                Destroy(this);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Start() is called before the first frame update.
     /// </summary>
     private void Start()
     {
-        image.texture = textures[0];
+        SetAnimation(startAnim);
+    }
+
+
+    // METHODS
+
+    /// <summary>
+    /// Animates the image over time using async.
+    /// </summary>
+    private async void Animate()
+    {
+        int animIndex = animCount;
+
+        while (true)
+        {
+            await Task.Delay((int)(1000 * (1 / frameRate)));
+
+            if (this == null || animIndex != animCount)
+            {
+                return;
+            }
+
+            ++this.animIndex;
+
+            this.animIndex %= currentAnim.frames.Count;
+
+            image.sprite = currentAnim.frames[this.animIndex];
+        }
     }
 
     /// <summary>
-    /// Animates the texture.
+    /// Sets the current animation to the given animation by name.
     /// </summary>
-    private void Update()
+    public void SetAnimation(string animName, int frame = 0)
     {
-        image.texture = textures[((int)index) % textures.Count];
+        animName = animName.ToLower();
 
-        index += Time.deltaTime * speed;
+        animIndex = 0;
+
+        currentAnim = null;
+
+        foreach (var anim in animations)
+        {
+            if (animName == anim.name.ToLower())
+            {
+                currentAnim = anim;
+
+                break;
+            }
+        }
+
+        if (currentAnim != null)
+        {
+            image.sprite = currentAnim.frames[frame % currentAnim.frames.Count];
+
+            ++animCount;
+
+            Animate();
+        }
+        else
+        {
+            Debug.LogError("ERROR: Animation \"" + animName + "\" was not found in the PlayerAnimaton component attached to " + gameObject.name + "!");
+
+            Destroy(this);
+        }
     }
+}
+
+/// <summary>
+/// A series of sprites used to create an animation on an image.
+/// </summary>
+[Serializable]
+public sealed class ImageAnimation
+{
+    [Header("A series of sprites used to create an animation on an image.\n")]
+
+    [Header("The name of this animation:")]
+    public string name = "New Animation";
+
+    [Header("Each sprite frame in this animation:")]
+    public List<Sprite> frames = new List<Sprite>();
 }

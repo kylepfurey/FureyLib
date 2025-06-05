f8e50e7bb4da6ce3c3f81393584ebd78da3607fb
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2025-06-05 09:43:20 -0400

    Update Main.ps1

--- FureyLib/Misc or Not Mine/Main.ps1
+++ FureyLib/Misc or Not Mine/Main.ps1
@@ -5,8 +5,8 @@
 function Main {
     param ([Parameter(ValueFromRemainingArguments=$true)][string[]]$Arguments)
     $Arguments = $Arguments -split ' '
-	Write-Output "Hello, Dinner!"
-	# New code here
+    Write-Output "Hello, Dinner!"
+    # New code here
     return "Program exited with code " + (0) + "."
 }
 

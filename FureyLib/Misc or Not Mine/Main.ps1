706172fa192e63bb1142e39fb2f2bb2658b3e3cf
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2025-06-05 09:42:23 -0400

    Add files via upload

---
+++ FureyLib/Misc or Not Mine/Main.ps1
@@ -0,0 +1,13 @@
+# .ps1
+# Program Main Entry Point Script
+
+# Entry point of the program.
+function Main {
+    param ([Parameter(ValueFromRemainingArguments=$true)][string[]]$Arguments)
+    $Arguments = $Arguments -split ' '
+	Write-Output "Hello, Dinner!"
+	# New code here
+    return "Program exited with code " + (0) + "."
+}
+
+Main $args

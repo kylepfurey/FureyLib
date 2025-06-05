7436e713832299a8beb2f4d6688ee5f830a5b5e1
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2025-06-05 11:06:08 -0400

    Add files via upload

---
+++ FureyLib/Misc or Not Mine/Main.vb
@@ -0,0 +1,25 @@
+' .vb
+' Program Main Entry Point Script
+
+Namespace Application
+
+	''' <summary>
+	''' The class representing the program that the application is running on.
+	''' </summary>
+	Public Class Program
+
+		''' <summary>
+		''' Entry point of the program.
+		''' </summary>
+		''' <param name="args"></param>
+		''' <returns>The return code of the program.</returns>
+		Public Shared Function Main(ByVal cmdArgs() As String) As Integer
+
+			Console.WriteLine("Hello, Dinner!")
+
+			' New code here
+
+			Return 0
+		End Function
+	End Class
+End Namespace

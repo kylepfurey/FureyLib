40ae22137784ce02fc6a1d782da774074f521941
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2025-06-05 11:22:30 -0400

    Add files via upload

---
+++ FureyLib/Misc or Not Mine/Visual Basic/Boilerplates/Class.vb
@@ -0,0 +1,34 @@
+' .vb
+' Name
+' Creator
+
+Imports System
+Imports System.Collections
+Imports System.Collections.Generic
+
+''' <summary>
+''' Template class script.
+''' </summary>
+Public Class NewClass
+
+	' VARIABLES
+
+	' New variables here
+
+
+	' CONSTRUCTORS
+
+	''' <summary>
+	''' Default constructor.
+	''' </summary>
+	Public Sub New()
+
+	End Sub
+
+	' New constructors here
+
+
+	' FUNCTIONS
+
+	' New functions here
+End Class

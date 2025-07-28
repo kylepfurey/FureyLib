94fd03c56685289498cca2af78f23dcb974e17a0
Kyle Furey <kylepfurey@gmail.com>
2025-07-28 08:22:00 -0400

    Java Boilerplate

---
+++ FureyLib/Misc or Not Mine/Java/Boilerplates/Class.java
@@ -0,0 +1,32 @@
+// .java
+// Name
+// Creator
+
+import java.util.*;
+
+/** Template class script. */
+public class Class {
+	// VARIABLES
+
+	// New variables here
+
+
+	// CONSTRUCTORS
+
+	/** Default constructor. */
+	public Class() {
+
+	}
+
+	/** Copy constructor. */
+	public Class(Class copied) {
+
+	}
+
+	// New constructors here
+
+
+	// METHODS
+
+	// New methods here
+}
---
+++ FureyLib/Misc or Not Mine/Java/Boilerplates/Main.java
@@ -0,0 +1,16 @@
+// .java
+// Program Main Entry Point Script
+
+import java.util.*;
+
+/** The class representing the program that the application is running on. */
+public class Main {
+	/** Entry point of the program. */
+	public static void main(String[] args) {
+		System.out.println("Hello, Dinner!");
+
+		// New code here
+
+		System.exit(0);
+	}
+}

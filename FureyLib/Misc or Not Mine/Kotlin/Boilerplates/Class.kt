5c026f12987e6b687f8c4f096004d73d7527f3eb
Kyle Furey <kylepfurey@gmail.com>
2026-03-21 18:44:58 -0400

    Kotlin

---
+++ FureyLib/Misc or Not Mine/Kotlin/Boilerplates/Class.kt
@@ -0,0 +1,32 @@
+// .kt
+// Name
+// Creator
+
+import kotlin.*;
+
+/** Template class script. */
+class Class {
+	// VARIABLES
+
+	// New variables here
+
+
+	// CONSTRUCTORS
+
+	/** Default constructor. */
+	constructor() {
+
+	}
+
+	/** Copy constructor. */
+	constructor(copied: Class) {
+
+	}
+
+	// New constructors here
+
+
+	// METHODS
+
+	// New methods here
+}
---
+++ FureyLib/Misc or Not Mine/Kotlin/Boilerplates/Main.kt
@@ -0,0 +1,13 @@
+// .kt
+// Program Main Entry Point Script
+
+import kotlin.*;
+
+/** Entry point of the program. */
+fun main(args: Array<String>) {
+	println("Hello, Dinner!");
+
+	// New code here
+
+	kotlin.system.exitProcess(0);
+}

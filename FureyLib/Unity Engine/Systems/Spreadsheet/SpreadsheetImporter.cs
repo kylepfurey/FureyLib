e53b07f5cba89e2ed00350f5ef8140c95e27ea99
Kyle Furey <112584997+kylepfurey@users.noreply.github.com>
2026-09-17 19:25:23 -0400

    Update SpreadsheetImporter.cs

--- FureyLib/Unity Engine/Systems/Spreadsheet/SpreadsheetImporter.cs
+++ FureyLib/Unity Engine/Systems/Spreadsheet/SpreadsheetImporter.cs
@@ -15,13 +15,13 @@ public static class SpreadsheetImporter
     {
         GSpreadSheetsToJson google = new();
         google.Init();                                                          // Field <spreadSheetKey> needs to be initialized
-        google.DownloadToJson();                                                // DownloadToJson() method needs to be public
+        google.DownloadToJson();                                                // Method DownloadToJson() needs to be public
         EditorUtility.ClearProgressBar();
         foreach (var sheetName in google.ranges)                                // Local variable <ranges> needs to be exposed as a public field
         {
-            string path = google.outputDir + sheetName;
-            string objFile = path + ".asset";                                   // Field <outputDir> needs to be initialized and public
-            string jsonFile = path + ".txt";                                    // Field <outputDir> needs to be initialized and public
+            string path = google.outputDir + sheetName;                         // Field <outputDir> needs to be initialized and public
+            string objFile = path + ".asset";
+            string jsonFile = path + ".txt";
             var sheet = AssetDatabase.LoadAssetAtPath<Spreadsheet>(objFile);
             sheet.Deserialize(File.ReadAllText(jsonFile));
             File.Delete(jsonFile);
